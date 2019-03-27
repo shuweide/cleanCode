831ea2242c70b35863b07002b1b907f81d230461
shuweide <shuweide@gmail.com>
2019-03-27 18:38:49 +0800

    add NioTest

--- src/main/java/idv/code/file/Nio2Test.java
+++ src/main/java/idv/code/file/Nio2Test.java
@@ -1,16 +1,25 @@
 package idv.code.file;
 
 import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.jar.Attributes;
 
 public class Nio2Test {
     public static void main(String[] args) {
-        File inputFile = new File("input.txt");
+        File inputFile = new File("src/main/resources/input.txt");
         try (InputStream in = new FileInputStream(inputFile)) {
-            Files.copy(in, Paths.get("output.txt"));
+            Files.copy(in, Paths.get("out/output.txt"), StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main2(String[] args) {
+        Path source, target;
+        Attributes attr;
+        Charset cs = Sta
     }
 }
