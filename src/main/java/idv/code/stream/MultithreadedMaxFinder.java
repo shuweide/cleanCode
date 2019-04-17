181247e25f4823f094c604fa2ce614600592595e
shuwei.kuo <shuweide@gmail.com>
2019-04-18 01:54:08 +0800

    add shutdown

--- src/main/java/idv/code/stream/MultithreadedMaxFinder.java
+++ src/main/java/idv/code/stream/MultithreadedMaxFinder.java
@@ -1,9 +1,6 @@
 package idv.code.stream;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class MultithreadedMaxFinder {
     public static int max(int[] data) throws ExecutionException, InterruptedException {
@@ -20,6 +17,9 @@ public class MultithreadedMaxFinder {
 
         Future<Integer> future1 = service.submit(task1);
         Future<Integer> future2 = service.submit(task2);
+
+        service.shutdown();
+        service.awaitTermination(5, TimeUnit.SECONDS);
 
         return Math.max(future1.get(), future2.get());
     }
