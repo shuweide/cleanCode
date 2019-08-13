b4cb7c06690564a303f4fcfdd8794e0eeeb1a460
shuweide <shuweide@gmail.com>
2019-08-13 17:36:46 +0800

    add Gzip to hex string

--- src/main/java/idv/code/stream/GZipInputStreamTest.java
+++ src/main/java/idv/code/stream/GZipInputStreamTest.java
@@ -2,15 +2,22 @@ package idv.code.stream;
 
 import java.io.*;
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 public class GZipInputStreamTest {
     public static void main(String[] args) {
 
         //Bytes String
-        String s = "1F8B";
-        byte[] bytes = new BigInteger(s,16).toByteArray();
+        String s = "TEST";
+
+        byte[] bytes = new BigInteger(s, 16).toByteArray();
         uncompressGZipToFile(bytes);
+        compressFileToGzipHexString();
     }
 
     private static void uncompressGZipToFile(byte[] bytes) {
@@ -25,5 +32,49 @@ public class GZipInputStreamTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String compressFileToGzipHexString() {
+
+        String output = "";
+        try (
+                Stream<String> stream = Files.lines(Paths.get("out/detail.json"), StandardCharsets.UTF_8);
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                GZIPOutputStream gos = new GZIPOutputStream(out);
+        ) {
+            StringBuilder sb = new StringBuilder();
+            stream.forEach(sb::append);
+
+            System.out.println(sb.toString());
+
+            gos.write(sb.toString().getBytes(StandardCharsets.UTF_8));
+            gos.close();
+
+            output = bytesToHex(out.toByteArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (OutputStream out = new BufferedOutputStream(new FileOutputStream("out/detail2.json"))) {
+            out.write(output.getBytes(StandardCharsets.UTF_8));
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(output);
+        return output;
+    }
+
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+
+    private static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 }
