207bc984f96ed1ad1e0c718dae28c3be50d3aa52
shuwei <shuwei.kuo@simbo.com.tw>
2019-09-19 17:05:03 +0800

    add first selenium test

---
+++ src/main/java/idv/code/selenium/FirstSelenium.java
@@ -0,0 +1,27 @@
+package idv.code.selenium;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+
+public class FirstSelenium {
+    public static void main(String[] args) throws InterruptedException {
+
+        File file = new File(FirstSelenium.class.getClassLoader().getResource("chromedriver.exe").getFile());
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("http://www.baidu.com");
+        WebElement searchInput = driver.findElement(By.name("wd"));
+        searchInput.sendKeys("极客时间");
+        searchInput.submit();
+
+        Thread.sleep(3000);
+        System.out.println(driver.getTitle());
+        Assert.assertEquals("极客时间_百度搜索", driver.getTitle());
+        driver.quit();
+    }
+}
